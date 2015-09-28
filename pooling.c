ec1a99479d526e8214027ecfeae2b152acb3b125
CorcovadoMing <rf37535@gmail.com>
2015-09-28 21:07:10 +0800

    Implement avg pooling

--- pooling.c
+++ pooling.c
@@ -8,7 +8,6 @@ image_t *max_pooling(image_t *target, int factor) {
 	result->element = (float*)malloc(result_size*result_size*sizeof(float));
 	
 	int i = 0, j = 0, m = 0, n = 0;
-
 	for(i = 0; i < result_size; i += 1) {
 		for(j = 0; j < result_size; j += 1) {
 			float max_element = -999;
@@ -24,3 +23,26 @@ image_t *max_pooling(image_t *target, int factor) {
 	}
 	return result;
 }
+
+
+image_t *avg_pooling(image_t *target, int factor) {
+	image_t *result = (image_t*)malloc(sizeof(image_t));
+	int result_size = target->size/factor;
+	result->size = result_size;
+	result->element = (float*)malloc(result_size*result_size*sizeof(float));
+	
+	int i = 0, j = 0, m = 0, n = 0;
+	for(i = 0; i < result_size; i += 1) {
+		for(j = 0; j < result_size; j += 1) {
+			float sum = 0;
+			for(m = 0; m < factor; m += 1) {
+				for(n = 0; n < factor; n += 1) {
+					sum += target->element[i*target->size*factor+j*factor+m*target->size+n];
+				}
+			}
+			result->element[i*result->size+j] = sum;
+		}
+	}
+	return result;
+}
+
