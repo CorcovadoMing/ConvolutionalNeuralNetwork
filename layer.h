a6a9d5fc19e8a449c188f8bc31bee917de8a93f4
CorcovadoMing <rf37535@gmail.com>
2016-03-26 20:49:04 +0800

    Layer is the collection of container type

---
+++ layer.h
@@ -0,0 +1,17 @@
+#ifndef __LAYER_T__
+#define __LAYER_T__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+	int size;
+	container_t *feature;
+} layer_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
