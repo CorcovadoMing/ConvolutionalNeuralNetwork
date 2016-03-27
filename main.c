b6a1657a4c26bca0dff54be95b38cd64b44fdcc5
CorcovadoMing <rf37535@gmail.com>
2016-03-27 14:36:23 +0800

    Add testcase of pooling

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
 
 	int i;
 	printf("==== Initial status ====\n");
-	image_t *test_cimage1 = init_image(3);
+	image_t *test_cimage1 = init_image(5);
 	for(i = 0; i < test_cimage1->size*test_cimage1->size; i += 1) {
 		if (i%2)
 			test_cimage1->element[i] = i;
@@ -18,7 +18,7 @@ int main() {
 			test_cimage1->element[i] = -i;
 	}
 
-	image_t *test_cimage2 = init_image(3);
+	image_t *test_cimage2 = init_image(5);
 	for(i = 0; i < test_cimage2->size*test_cimage2->size; i += 1) {
 		if (i%2)
 			test_cimage2->element[i] = i;
@@ -60,9 +60,15 @@ int main() {
 	printlayer(result_alayer);
 
 	printf("==== Test pooling ====\n");
-	//int pooling_factor = 3;
-	//image_t *result_pimage = max_pooling(result_aimage, pooling_factor);
-	//printmap(result_pimage);
+	printf("Max Pooling:\n");
+	layer_t *result_player = max_pooling(result_alayer, 3);
+	printlayer(result_player);
+	printf("Avg Pooling:\n");
+	result_player = avg_pooling(result_alayer, 3);
+	printlayer(result_player);
+	printf("Min Pooling:\n");
+	result_player = min_pooling(result_alayer, 3);
+	printlayer(result_player);
 
 	return 0;
 }
